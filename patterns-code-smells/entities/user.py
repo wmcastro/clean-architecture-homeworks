6319a2d4fe8ca008c16e983ebcd1a21b6f1138ac
William Castro <wmcjimenez@gmail.com>
2024-04-07 10:04:13 -0500

    feat: :sparkles: Add User entity

---
+++ patterns-code-smells/entities/user.py
@@ -0,0 +1,9 @@
+# TODO: add print with columns
+class User:
+    def __init__(self):
+        print('Info about your entity:')
+        id = 'id'
+        name = 'Name'
+        age = 'age'
+        email = 'email'
+        country = 'country'
